e23ff186d97bd9795d37f5367ff834f8d698bb7a
zvirot1 <zvirot@gmail.com>
2023-11-20 08:25:48 +0200

    aaa

--- src/main/java/com/gamezone/jwt/security/SecurityConfiguration.java
+++ src/main/java/com/gamezone/jwt/security/SecurityConfiguration.java
@@ -22,7 +22,7 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-         http.headers().frameOptions().disable();
+        // http.headers().frameOptions().disable();
         return http
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
